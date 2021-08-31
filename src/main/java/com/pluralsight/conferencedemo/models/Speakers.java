2f6537da103ed700343ba0a9b8da39f36b384cd8
Moira Leon <moira.leon2@gmail.com>
2021-08-31 16:41:35 -0600

    revising Speakers.java data types

--- src/main/java/com/pluralsight/conferencedemo/models/Speakers.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Speakers.java
@@ -4,68 +4,68 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.io.Serial;
+
 
 @Entity(name ="speakers")
 public class Speakers {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Serial speaker_id;
-    private Character first_name;
-    private Character last_name;
-    private Character title;
-    private Character company;
-    private Character speaker_bio;
+    private Long speaker_id;
+    private String first_name;
+    private String last_name;
+    private String title;
+    private String company;
+    private String speaker_bio;
 
     public Speakers(){
 
     }
 
-    public Serial getSpeaker_id() {
+    public Long getSpeaker_id() {
         return speaker_id;
     }
 
-    public void setSpeaker_id(Serial speaker_id) {
+    public void setSpeaker_id(Long speaker_id) {
         this.speaker_id = speaker_id;
     }
 
-    public Character getFirst_name() {
+    public String getFirst_name() {
         return first_name;
     }
 
-    public void setFirst_name(Character first_name) {
+    public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
 
-    public Character getLast_name() {
+    public String getLast_name() {
         return last_name;
     }
 
-    public void setLast_name(Character last_name) {
+    public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
 
-    public Character getTitle() {
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle(Character title) {
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public Character getCompany() {
+    public String getCompany() {
         return company;
     }
 
-    public void setCompany(Character company) {
+    public void setCompany(String company) {
         this.company = company;
     }
 
-    public Character getSpeaker_bio() {
+    public String getSpeaker_bio() {
         return speaker_bio;
     }
 
-    public void setSpeaker_bio(Character speaker_bio) {
+    public void setSpeaker_bio(String speaker_bio) {
         this.speaker_bio = speaker_bio;
     }
 }
